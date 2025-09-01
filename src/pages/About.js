f53489100a26767a892631e864a634d360debf71
ChristopheCoustillas <christophe.coustillas@oclock.school>
2025-09-01 13:42:21 +0200

    projet react world

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel laborum
+        dolor saepe, nostrum odio, est, repellat quam aut dicta nam quos numquam
+        voluptatem neque? Illo, debitis dignissimos iure numquam assumenda
+        illum, doloribus inventore pariatur doloremque accusamus optio quas
+        earum qui praesentium esse dolorem, asperiores perspiciatis explicabo
+        ipsam. Architecto porro explicabo quis quaerat cupiditate minus, maiores
+        repellat officiis nihil. Ad eligendi dolorem esse dicta odio maiores
+        modi fugit corporis id magnam asperiores nesciunt porro repudiandae
+        libero exercitationem ducimus, nam deserunt. Amet vel provident numquam
+        quo doloremque, recusandae placeat nobis impedit sed voluptatem debitis
+        autem repudiandae officiis nisi porro illum optio ut!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+        distinctio quidem, sit sint facilis eveniet magni aut beatae temporibus
+        perferendis hic alias nisi illum! Nihil quisquam quod tenetur sunt eius
+        ad minima rerum? Officiis nostrum rerum sapiente possimus. Praesentium
+        deserunt sapiente dolorum voluptas ex ipsa tenetur magni quia, porro
+        aliquam ad sed ducimus quis exercitationem nobis. Placeat illum rerum
+        consequatur, laudantium porro nostrum dignissimos mollitia? Dignissimos
+        aut consequuntur dolore libero culpa autem sunt, cupiditate molestiae,
+        corporis ut rerum debitis non nihil maiores odio minima beatae velit
+        deleniti totam ipsum unde nobis dolorem hic ab. Maiores laboriosam
+        magnam sequi, assumenda nesciunt suscipit autem numquam porro, illum
+        aut, reprehenderit eos molestias quo adipisci. Maxime in dolorem atque
+        culpa accusamus ad non ducimus nihil earum necessitatibus possimus
+        sequi, maiores, dolor, aliquam magnam mollitia dicta iste modi veritatis
+        voluptas vero. Quae, quod, ullam, recusandae impedit distinctio
+        exercitationem aperiam nihil eum reprehenderit iusto consectetur.
+        Officia, corrupti minus recusandae quasi fuga similique quos at libero
+        non ipsam praesentium commodi debitis illo hic eveniet laudantium.
+        Soluta omnis optio corrupti fugiat eligendi minus, rem dolor in. Sed
+        quae exercitationem ea veniam veritatis nobis odio, animi accusantium
+        quidem fuga similique beatae laudantium harum velit temporibus? Impedit
+        saepe omnis similique?
+      </p>
+    </div>
+  );
+};
+
+export default About;
