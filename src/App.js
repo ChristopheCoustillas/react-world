e64fa826c4c21d6a0cb4abf25578e13532870874
ChristopheCoustillas <christophe.coustillas@oclock.school>
2025-09-02 10:25:27 +0200

    ajout page blog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
         {/*path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au-dessus */}
         <Route path="*" element={<Home />} />
       </Routes>
